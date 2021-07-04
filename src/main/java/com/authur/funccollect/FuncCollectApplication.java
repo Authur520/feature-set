9d702b6932732d4e25dd78592846ef007db93d4e
jibing.li <jibing.li@youbiai.com>
2021-07-04 18:04:07 +0800

    frist commit project

---
+++ src/main/java/com/authur/funccollect/FuncCollectApplication.java
@@ -0,0 +1,13 @@
+package com.authur.funccollect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuncCollectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuncCollectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/authur/feature/set/funccollect/FuncCollectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.authur.feature.set.funccollect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuncCollectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
