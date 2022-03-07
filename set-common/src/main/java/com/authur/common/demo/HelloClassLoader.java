6b8bd0b46a6cfb50be89ef5995ef058c0b551509
jibing.li <jibing.li@youbiai.com>
2022-03-07 21:42:00 +0800

    common

---
+++ set-common/src/main/java/com/authur/common/demo/HelloClassLoader.java
@@ -0,0 +1,15 @@
+package com.authur.common.demo;
+
+/**
+ * @Description:
+ * @Author: jibing.Li
+ * @Date: 2022/3/7 17:21
+ */
+public class HelloClassLoader extends ClassLoader{
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+
+
+}
